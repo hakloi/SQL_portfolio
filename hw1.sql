a47d76eefad8f4fea60faaefd55b76effc2958e2
Violetta <murasakitta@gmail.com>
2023-07-13 22:21:28 +0300

    first hw update

--- hw1.sql
+++ hw1.sql
@@ -20,3 +20,36 @@ WHERE product_count > 2;
 SELECT product_name, manufacturer, product_count, price
 FROM mobile_phones
 WHERE manufacturer = "Samsung";
+
+-- 4 (по желанию)* С помощью регулярных выражений найти:
+-- 4.1. Товары, в которых есть упоминание "Iphone"
+SELECT
+*
+FROM
+mobile_phones
+WHERE 
+REGEXP_LIKE(product_name, 'Iphone');
+
+-- 4.2. Товары, в которых есть упоминание "Samsung"
+SELECT
+*
+FROM
+mobile_phones
+WHERE 
+REGEXP_LIKE(manufacturer, 'Samsung');
+
+-- 4.3. Товары, в которых есть ЦИФРЫ
+SELECT
+*
+FROM
+mobile_phones
+WHERE 
+product_name REGEXP '[[:digit:]]' = 1 || manufacturer REGEXP '[[:digit:]]' = 1;
+
+-- 4.4. Товары, в которых есть ЦИФРА "8"
+SELECT
+*
+FROM
+mobile_phones
+WHERE 
+product_name REGEXP '[[8]]' = 1;
